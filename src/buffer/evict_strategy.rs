7f6073f0742399df7c1ddd040c9f9d032950fb2a
Shuntaro Shimizu <ut.s.shimizu@gmail.com>
2023-06-25 11:16:44 +0900

    trait

--- src/buffer/evict_strategy.rs
+++ src/buffer/evict_strategy.rs
@@ -1,1 +1,5 @@
+use super::core::{BufferId, Frame};
 
+pub trait EvictStrategy {
+    fn evict(pool_size: usize, victim_id: BufferId, buffers: Vec<Frame>) -> BufferId;
+}
--- src/buffer/mod.rs
+++ src/buffer/mod.rs
@@ -2,3 +2,4 @@ mod buffer_pool;
 pub mod buffer_pool_manager;
 mod core;
 mod error;
+mod evict_strategy;
