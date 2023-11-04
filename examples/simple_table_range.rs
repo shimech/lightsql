64bd897922f6d82392ee8337c7c550df7e1a9da0
Shuntaro Shimizu <ut.s.shimizu@gmail.com>
2023-11-04 22:25:04 +0900

    range

---
+++ examples/simple_table_range.rs
@@ -0,0 +1,26 @@
+use anyhow::Result;
+use lightsql::{
+    btree::{BTree, SearchMode},
+    buffer::{BufferPool, BufferPoolManager},
+    disk::{DiskManager, PageId},
+    tuple,
+};
+
+fn main() -> Result<()> {
+    let disk = DiskManager::open("simple.lsql")?;
+    let pool = BufferPool::new(10);
+    let mut bufmgr = BufferPoolManager::new(disk, pool);
+
+    let btree = BTree::new(PageId(0));
+    let mut search_key = vec![];
+    tuple::encode([b"y"].iter(), &mut search_key);
+    let mut iter = btree.search(&mut bufmgr, SearchMode::Key(search_key))?;
+
+    while let Some((key, value)) = iter.next(&mut bufmgr)? {
+        let mut record = vec![];
+        tuple::decode(&key, &mut record);
+        tuple::decode(&value, &mut record);
+        println!("{:?}", tuple::Pretty(&record));
+    }
+    Ok(())
+}
